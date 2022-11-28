9c8e9f4733be4df1f64152ad9da54a19ce6cebf9
Alejandro Gomez <73868258+agosmou@users.noreply.github.com>
2022-11-27 20:37:16 -0800

    HW9_1 & HW9_2
    finished the first 1 - the second is en route

--- HW9_1/src/HW9_1.java
+++ HW9_1/src/HW9_1.java
@@ -1,54 +1,84 @@
 public class HW9_1 {
    public static void main(String[] args) {
+	   
       Solution sol = new Solution();
       // Your solution method will be tested with random input 
       // such as the following; the actual test cases will be 
       // different
       sol.put(1, 1); 
       sol.put(2, 3); 
-      sol.get(1);       // returns 1 
-      sol.get(30);      // returns -1 (not found) 
+      System.out.println(" 1 -> " + sol.get(1));       // returns 1 
+      System.out.println(" -1 -> " + sol.get(30));      // returns -1 (not found) 
       sol.put(1, 100);  // update the existing value 
-      sol.get(1);       // returns 100 
+      System.out.println(" 100 -> " + sol.get(1));       // returns 100 
       sol.remove(2);    // removes the mapping for 2
       sol.remove(1);    // removes the mapping for 1 
-      sol.get(2);       // returns -1 (not found)
-      sol.get(1);       // returns -1 (not found) 
+      System.out.println(" -1 -> " + sol.get(2));       // returns -1 (not found)
+      System.out.println(" -1 -> " + sol.get(1));       // returns -1 (not found) 
    }
 } 
 
 class Solution {
 
    // You may add any global variables you need here 
+	
+	int[] hashmapArray; // initialize integer array
 
    public Solution() { 
       // YOUR CODE HERE, IF NEEDED
+	   
+	   hashmapArray = new int[10000];  // create array to hold 10,000 elements. Empty arrays default to "0" values     
+
+	   // Use for-loop to fill the array indexes to "-1" for all elements
+	   for (int i = 0; i < hashmapArray.length; i++) {
+
+		   hashmapArray [i] = -1;
+	   }
+	   
    } 
 
    /**
-   * PURPOSE: 
-   * PARAMETERS: 
-   * RETURN VALUES:
+   * PURPOSE: To create a map item with a key (i.e. assign an array index to an integer element)
+   * PARAMETERS: key value and the integer value
+   * RETURN VALUES: void method will not return value
    */ 
    public void put(int key, int value) { 
       // YOUR CODE HERE 
+	   hashmapArray[key] = value;
+	   
    } 
 
    /**
-   * PURPOSE: 
-   * PARAMETERS: 
-   * RETURN VALUES:
+   * PURPOSE: to print the map value for the given key (i.e. print the integer element of an array index)
+   * PARAMETERS: key value
+   * RETURN VALUES: Return value is an integer value for the requested array index
    */    
    public int get(int key) { 
       // YOUR CODE HERE 
+	   return hashmapArray[key];
    } 
 
    /**
-   * PURPOSE: 
-   * PARAMETERS: 
-   * RETURN VALUES:
+   * PURPOSE: return a value to show a non-existing map item (i.e. reassign a -1 element integer to a given index)
+   * PARAMETERS: key value
+   * RETURN VALUES: void method will not return value
    */ 
    public void remove(int key) { 
       // YOUR CODE HERE 
+	   hashmapArray[key] = -1;
    } 
 }
+
+
+/*
+-You are NOT allowed to pull in any native Java hash table libraries like HashMap, Map, Hashtable, etc., from java.util to complete this problem; doing so receives -5 logic points.  You may however make use of any non-HashMap related libraries in java.util for this problem.
+-You may use any native integer array or your custom LinkedList Node class from our previous lectures as your starting data structure of choice for this problem. If you include a class from our lectures, please make sure you include that class in the same Java solution you submit.
+-It is completely up to you as to how your logic is implemented to deliver the required outcome. 
+-The size of your hashmap will not exceed 10,000 elements.
+-Both keys and values are within the allowable integer ranges of Integer.MAX_VALUE and Integer.MIN_VALUE.
+-For this problem, do not worry about handling exceptions; meaning, for example, you won't have to handle use cases that lead to an error in operation, such as removing an element from an empty HashMap. 
+
+Hints
+-Think simple. This problem can be solved with either a linked list of your custom implementation or an array.
+-You are allowed to create any additional methods of your choice. Caveat: you CANNOT modify the provided method signatures or your solution will not pass autograder.
+*/
--- HW9_2/src/HW9_2.java
+++ HW9_2/src/HW9_2.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class HW9_2 {
    public static void main(String[] args) {
       Solution sol = new Solution();
@@ -7,10 +9,10 @@ public class HW9_2 {
       sol.put("app", 100); 
       sol.getSum("ap");     // returns 100 as "ap" is a prefix  to "app"
       sol.put("ap", 1);
-      sol.getSum("a");      // returns 101 as "a" is a prefix to both keys in map
-      sol.getSum("xyz");    // returns 0 as "xyz" is not a prefix to any keys in map 
+      System.out.println("101 == " + sol.getSum("a"));      // returns 101 as "a" is a prefix to both keys in map
+      System.out.println("0 == " + sol.getSum("xyz"));    // returns 0 as "xyz" is not a prefix to any keys in map 
       sol.put("app", 8);    // replaces the value of the existing key "app" from 100 to 8
-      sol.getSum("ap");     // returns 9 as "ap" is a prefix to both keys in map 
+      System.out.println("9 == " + sol.getSum("ap"));     // returns 9 as "ap" is a prefix to both keys in map 
    }
 } 
 
@@ -18,26 +20,45 @@ public class HW9_2 {
 class Solution {
 
    // You may add any global variables here
+	
+	Map <String, Integer> myMap; // initialize
 
    public Solution() { 
       // YOUR CODE HERE, IF NEEDED 
+	   
+	   myMap = new HashMap<String,Integer>(); // constructor
    } 
 
    /**
-   * PURPOSE: 
-   * PARAMETERS: 
-   * RETURN VALUES:
+   * PURPOSE: inserts a key,value pair into the map
+   * PARAMETERS: parameter passed is the string key and integer value
+   * RETURN VALUES: the void method will not return any values
    */ 
    public void put(String key, int value) { 
       // YOUR CODE HERE 
+	   myMap.put(key, value);
    } 
 
    /**
-   * PURPOSE: 
-   * PARAMETERS: 
-   * RETURN VALUES:
+   * PURPOSE: to find the sum of values associated with the string key
+   * PARAMETERS: the parameter passed is a string
+   * RETURN VALUES: will return an int sum of the values associated with the key
    */ 
    public int getSum(String key) { 
       // YOUR CODE HERE 
+	   
+	   
+	   
+	   
    } 
 }
+/*
+*Constraints and Assumptions
+-You are allowed to use any data structures via import java.util.* for this problem.
+-For the put method, the input key is an English prefix (String), and the value is a non-negative integer.
+-For the getSum method, the input key is a non-empty String.
+-It is completely up to you as to how your logic is implemented to deliver the required outcome. 
+-The size of your hashmap will not exceed 10,000 elements.
+-The values are within allowable integer ranges of Integer.MAX_VALUE and Integer.MIN_VALUE.
+-The length of your English prefix is from 1 to 50 characters inclusive. Note, this means an empty string, "", cannot be a valid prefix because its length is 0. 
+*/
