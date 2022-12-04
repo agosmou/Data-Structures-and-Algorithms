ab9beef73d971174a5993e2f959320622555f5ae
Alejandro Gomez <73868258+agosmou@users.noreply.github.com>
2022-12-04 06:40:27 -0800

    Update HW10_2.java
    set up to properly begin with test cases

--- HW10_2/src/HW10_2.java
+++ HW10_2/src/HW10_2.java
@@ -10,12 +10,21 @@ public class HW10_2 {
       int[] output = sol.getN(new int[]{1,2,3,4,5,6}, 3);
 
       // You may use this approach to print your output to console to validate
-      System.out.println(Arrays.toString(output)); // combination of any 3 input integers
+      System.out.println("[2,3,6], [1,2,3], [4,5,6], etc. => "+Arrays.toString(output)); // combination of any 3 input integers
                                                    // [2,3,6], [1,2,3], [4,5,6], etc. 
 
       // If you wish to repeat with a different test case, you may as follows:
       output = sol.getN(new int[]{1,1,1,2,2,3,3,3,3,3,3,4,4,4,4,4,4,4,4,5,5,6}, 3);
-      System.out.println(Arrays.toString(output)); // [6,5,2] or them 3 in any order
+      System.out.println("[6,5,2] in any order => "+Arrays.toString(output)); // [6,5,2] or them 3 in any order
+      
+      output = sol.getN(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2}, 1);
+      System.out.println("[2] => "+Arrays.toString(output)); // [2]       
+      
+      output = sol.getN(new int[]{1}, 1);
+      System.out.println("[1] => "+Arrays.toString(output)); // [1]  
+      
+      output = sol.getN(new int[]{11,12,1,2,10,100}, 3);
+      System.out.println("Any combination of 3 from array => "+Arrays.toString(output)); // Any combination of 3 from array 
    }
 } 
 
@@ -30,9 +39,42 @@ class Solution {
    */ 
    public int[] getN(int[] input, int N) {
       // YOUR CODE HERE 
+
+	   
+	   
+	   /*PSEUDOCODE
+	    * You may consider building a HashMap that stores each integer's value and its corresponding frequency first. 
+	    * This hash map is your dictionary of key-value pairs that give you each integer's number of occurrences in the input.
+	    * Then, use that HashMap to build a heap of size N. Will this heap be a min or max heap? 
+	    * At the end, your new heap should only have N integers
+	    * You will get these N integers from the heap into your output array to return; these N integers can be in any order in your output array.
+	    * Lastly: sometimes, you may have a test case where all of the integers in input occur exactly for the same number of times. 
+	    * For example: [1,1,2,2,3,3,4,4] and N=2. 
+	    * In this case, your output array of size 2 can be any combination of these integers.
+	    */
+	   
+	   
 	   
 	   Map<Integer,Integer> hash = new HashMap<Integer,Integer>();
 	   
+	   // am experiment - try printing hashmap
+	   
+	   int size = input.length;
+	   
+	   int i = 1;
+	   int j = 1;
+	   int k = 1;
+	   
+	   return new int[] {i,j,k};
 	   
    }
 }
+
+/*
+ * Constraints and Assumptions
+-1 <= input.length <= 10000.
+-Input integers are in the range of Integer.MIN_VALUE and Integer.MAX_VALUE.
+-Input integers do not have to be in any order.
+-1 <= N <= # of unique integers in input. For example, your input cannot be [1,2,2,3,3,3,10,11,12,100] 
+while N = 12 because the number of unique integers (7) in the input is less than N (12).
+*/
