31a6df48884b30f38325cecdd869e192acd6c9ef
Alejandro Gomez <73868258+agosmou@users.noreply.github.com>
2022-12-16 16:52:53 -0500

    Submitting HW_11

--- HW11_1/src/HW11_1.java
+++ HW11_1/src/HW11_1.java
@@ -15,12 +15,32 @@ public class HW11_1 {
 }
 
 /**
-* PURPOSE: 
-* PARAMETERS: 
-* RETURN VALUES:
+* PURPOSE: Find the string thats the source sink which is evaluated in boolean condition in main method
+* PARAMETERS: List of lists created in main method
+* RETURN VALUES: string value
 */ 
 class Solution {
    public String getDestination(List<List<String>> routes) {
       // YOUR CODE HERE
+	   
+	   // create hashmap to create adjacency list
+	   Map<String, String> adjList = new HashMap<>();
+	   
+	   for (List<String> route : routes) {
+		   adjList.put(route.get(0), route.get(1));
+	   }
+	   
+	   // Iterate through the entries to find the value that is NOT also a key. return this value as the sourcesink
+	   for (Map.Entry<String, String> entry : adjList.entrySet()) {
+		   if (!adjList.containsKey(entry.getValue())) {
+			   // check to return the source sink
+			   //System.out.println(entry.getValue());
+			   return entry.getValue();
+			   
+		   }
+	   }
+	   
+	   // return null typically when input routes list length is 0
+	   return null;
    }
 }
